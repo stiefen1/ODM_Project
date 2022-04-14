c139edfcb98e7780b97360cc5945864c1a432b4a
stiefen1 <stephen-monnet@hotmail.com>
2022-04-14 17:36:04 +0200

    First commit
    Add Matlab codes :
    - p11.m completed
    - p12.m completed
    Skeletons of p22, p23, p35

---
+++ p23.m
@@ -0,0 +1,96 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% EPFL | MGT-483: Optimal Decision Making | Group Project, Exercise 2.3 %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all; close all; yalmip('clear');clc;
+%% Data
+% The following data is define as a struct for each generator.
+% You could define it in your own way if you want, 
+% like vectors: GenCost=[15; 20; 15; 20; 30; 25];
+
+% number of generators
+NGen= 6;
+% time duration
+T=24;
+% generation cost
+G1.cost = 15;
+G2.cost = 20;
+G3.cost = 15;
+G4.cost = 20;
+G5.cost = 30;
+G6.cost = 25;
+% capacity
+G1.capacity = 10;
+G2.capacity = 5;
+G3.capacity = 10;
+G4.capacity = 10;
+G5.capacity = 20;
+G6.capacity = 30;
+% start-up cost
+G1.startup = 75;
+G2.startup = 100;
+G3.startup = 75;
+G4.startup = 100;
+G5.startup = 100;
+G6.startup = 125;
+% shutdown cost
+G1.shutdown = 7.5;
+G2.shutdown = 10;
+G3.shutdown = 17.5;
+G4.shutdown = 10;
+G5.shutdown = 10;
+G6.shutdown = 12.5;
+% no-load cost
+G1.noload = 10;
+G2.noload = 5;
+G3.noload = 10;
+G4.noload = 10;
+G5.noload = 10;
+G6.noload = 10;
+% initial state of generators x_0
+G1.inital = 1;
+G2.inital = 0;
+G3.inital = 0;
+G4.inital = 0;
+G5.inital = 0;
+G6.inital = 0;
+% minimum up-time of generators
+G1.minup = 3;
+G2.minup = 3;
+G3.minup = 3;
+G4.minup = 3;
+G5.minup = 3;
+G6.minup = 3;
+% minimum down-time of generators
+G1.mindown = 2;
+G2.mindown = 2;
+G3.mindown = 2;
+G4.mindown = 2;
+G5.mindown = 2;
+G6.mindown = 2;
+
+% renewable energy source
+r=[15.2;16.4;16.1;10.9;14.8;7.6;15.6;5.5;9.2;5.7;1.5;12.4;10.4;4.8;14.3;0.5;6.6;5.7;11.5;11.9;2.8;7.3;6.7;9.7];
+
+% demand
+d=[21.3;21.4;17.8;20.9;15.5;17.6;20.2;23.8;27.7;30.1;35.4;39.4;43.2;47.0;49.3;51.5;52.6;50.3;47.0;43.1;38.8;33.2;28.6;24.3];
+
+%% Unit commitment
+% Hints: define binary variables (vector) in yalmip:  
+% a = binvar(N,M) with dimension N*M 
+
+% three elements
+%variables Initial
+
+con=[];%constraints initial
+obj=0;%objective function initial
+
+% objective function
+
+% constraints
+
+%% define sdpsetting
+ops=sdpsettings('solver','LPSOLVE');
+sol=solvesdp(con,obj,ops);
+solvertime=sol.solvertime;
+
+% obtain the solutions and objective value
